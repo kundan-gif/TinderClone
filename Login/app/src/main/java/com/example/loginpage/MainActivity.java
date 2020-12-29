81300be6afb1937766188b860700ffb55dc86934
pravin5551 <pravinvpawar5551@gmail.com>
2020-12-29 16:01:09 +0530

    homepage button

--- Login/app/src/main/java/com/example/loginpage/MainActivity.java
+++ Login/app/src/main/java/com/example/loginpage/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.loginpage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnLaunchHomepage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initilizationAndLisner();
+    }
+
+    private void initilizationAndLisner() {
+        btnLaunchHomepage = findViewById(R.id.btnGoogle);
+        btnLaunchHomepage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this ,homepage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
