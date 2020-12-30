16425e4734fcf11823f91b3b5ace5e9f895e99f4
pravin5551 <pravinvpawar5551@gmail.com>
2020-12-30 12:02:54 +0530

    connect

--- Login/app/src/main/java/com/example/loginpage/homepage.java
+++ Login/app/src/main/java/com/example/loginpage/homepage.java
@@ -2,13 +2,49 @@ package com.example.loginpage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class homepage extends AppCompatActivity {
+    private ImageButton btnlikes;
+    private ImageButton btnmessges;
+    private ImageButton btnaccount;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+        initilizationAndListnerr();
+    }
+
+    private void initilizationAndListnerr() {
+       btnlikes = findViewById(R.id.likes);
+       btnmessges = findViewById(R.id.messges);
+       btnaccount = findViewById(R.id.account);
+       btnlikes.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Intent intent = new Intent(homepage.this ,likespage.class);
+               startActivity(intent);
+           }
+       });
+       btnmessges.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Intent intent = new Intent(homepage.this , messegepage.class);
+               startActivity(intent);
+           }
+       });
+        btnaccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(homepage.this , accountpage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
