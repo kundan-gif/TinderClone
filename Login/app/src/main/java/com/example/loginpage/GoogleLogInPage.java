4fa1a64ac66038b2582ee77c5aebcc43478a3b9c
pravin5551 <pravinvpawar5551@gmail.com>
2020-12-31 18:52:02 +0530

    Log in pages

---
+++ Login/app/src/main/java/com/example/loginpage/GoogleLogInPage.java
@@ -0,0 +1,30 @@
+package com.example.loginpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class GoogleLogInPage extends AppCompatActivity {
+    private Button btnGoogle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google_log_in_page);
+        intilizationandListner();
+    }
+
+    private void intilizationandListner() {
+        btnGoogle = findViewById(R.id.btnGoogleuser);
+        btnGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(GoogleLogInPage.this , homepage.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
