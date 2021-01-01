76c43d8e0c5987377b7d55866e32968ab8ad2a6e
Kundan Kumar <kundan211097@gmail.com>
2021-01-01 14:04:42 +0530

    Tinder Plus and Activity Communication

--- Login/app/src/main/java/com/example/loginpage/homepage.java
+++ Login/app/src/main/java/com/example/loginpage/homepage.java
@@ -7,11 +7,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class homepage extends AppCompatActivity {
     private ImageButton btnlikes;
     private ImageButton btnmessges;
     private ImageButton btnaccount;
+    private ImageButton mImgBtnReload;
+    private ImageButton mImgBtnBoost;
+    private ImageButton mImgBtnCross;
+    private ImageButton mImgBtnStar;
+    private ImageButton mImgBtnLike;
 
 
     @Override
@@ -25,6 +31,11 @@ public class homepage extends AppCompatActivity {
        btnlikes = findViewById(R.id.likes);
        btnmessges = findViewById(R.id.messages);
        btnaccount = findViewById(R.id.account);
+       mImgBtnReload=findViewById(R.id.iBtnReload);
+       mImgBtnBoost=findViewById(R.id.imgBtnBoost);
+       mImgBtnCross=findViewById(R.id.imgBtnCross);
+       mImgBtnStar=findViewById(R.id.imgBtnStar);
+       mImgBtnLike=findViewById(R.id.imgBtnLike);
        btnlikes.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View v) {
@@ -44,6 +55,38 @@ public class homepage extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(homepage.this , accountpage.class);
                 startActivity(intent);
+            }
+        });
+        mImgBtnReload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(homepage.this , TinderPlus.class);
+                startActivity(intent);
+            }
+        });
+        mImgBtnBoost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(homepage.this , BoostActivity.class);
+                startActivity(intent);
+            }
+        });
+        mImgBtnCross.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(homepage.this,"NOPE", Toast.LENGTH_SHORT).show();
+            }
+        });
+        mImgBtnStar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(homepage.this,"SUPER LIKE", Toast.LENGTH_SHORT).show();
+            }
+        });
+        mImgBtnLike.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(homepage.this,"LIKE", Toast.LENGTH_SHORT).show();
             }
         });
     }
